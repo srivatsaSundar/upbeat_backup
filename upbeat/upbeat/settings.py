079872d6deda84a2f9d6acf3bb165b09f1a6913d
Srivatsa S <srivatsas0503@gmail.com>
2024-09-21 22:36:09 +0530

    csrf main

--- upbeat/upbeat/settings.py
+++ upbeat/upbeat/settings.py
@@ -136,6 +136,6 @@ SESSION_ENGINE = 'django.contrib.sessions.backends.db'  # Default
 
 CORS_ALLOW_ALL_ORIGINS = True
 
-CORS_ALLOWED_ORIGINS = ['https://upbeat-backup.onrender.com','http://localhost:3000/','https://upbeat-mu.vercel.app/','https://main.d2xz3cyvwx48d0.amplifyapp.com/']
+CORS_ALLOWED_ORIGINS = ['*','https://upbeat-backup.onrender.com','http://localhost:3000','https://upbeat-mu.vercel.app/','https://main.d2xz3cyvwx48d0.amplifyapp.com/']
 
-CSRF_TRUSTED_ORIGINS=['https://upbeat-backup.onrender.com','http://localhost:3000/','https://upbeat-mu.vercel.app/','https://main.d2xz3cyvwx48d0.amplifyapp.com/']
+# CSRF_TRUSTED_ORIGINS=['https://upbeat-backup.onrender.com','http://localhost:3000/','https://upbeat-mu.vercel.app/','https://main.d2xz3cyvwx48d0.amplifyapp.com/']
